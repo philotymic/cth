2852de12b2fa22bed39e66ab1e2d8c95877b1404
Gogs <gogs@fake.local>
2019-11-07 05:16:40 +0000

    qt-client works with py-server

--- examples/transport-xhr/qt-client/mainwindow.cpp
+++ examples/transport-xhr/qt-client/mainwindow.cpp
@@ -1,14 +1,15 @@
 #include "mainwindow.h"
 #include <QCoreApplication>
 
-/*
-#include <thrift/protocol/TBinaryProtocol.h>
-#include <thrift/transport/TSocket.h>
-#include <thrift/transport/TTransportUtils.h>
-#include <thrift/stdcxx.h>
+#include <thrift/transport/THttpClient.h>
+#include <thrift/protocol/TJSONProtocol.h>
 
 #include "gen-cpp/Hello.h"
-*/
+
+using namespace hello;
+using namespace std;
+using namespace apache::thrift::transport;
+using namespace apache::thrift::protocol;
 
 MainWindow::MainWindow(QWidget *parent)
   : QMainWindow(parent)
@@ -20,21 +21,14 @@ MainWindow::MainWindow(QWidget *parent)
  
 void MainWindow::handleButton()
 {
-  /*
-  HelloClient* hello;
-  stdcxx::shared_ptr<TTransport> socket(new TSocket("localhost", 9090));
-  stdcxx::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-  stdcxx::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
-  CalculatorClient client(protocol);
-  transport->open();
-  client.ping();
-  cout << "ping()" << endl;
-  
-  
+  auto transport = make_shared<THttpClient>("localhost", 9090, "/");
+  auto protocol = make_shared<TJSONProtocol>(transport);
 
-  std::string res; hello.sayHello(res);
-  m_button->setText(res.c_str());
-  */
-  m_button->setText("TEST123");
+  HelloClient client(protocol);
+  transport->open();
+  std::string res; client.sayHello(res);
+  transport->close();
+  
+  m_button->setText(res.c_str());  
   m_button->resize(100, 100);
 }
