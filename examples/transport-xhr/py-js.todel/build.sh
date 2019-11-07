d6723af75daca65e1f3ba31323c21ff6fa128b7a
Gogs <gogs@fake.local>
2019-11-07 02:10:11 +0000

    old code removed

--- examples/transport-xhr/py-js.todel/build.sh
+++
@@ -1,4 +0,0 @@
-rm -rf gen-py src/gen-js
-thrift --gen py ../../Hello.thrift
-(cd src && thrift --gen js:es6 ../../../Hello.thrift)
-
--- examples/transport-xhr/py-js.todel/clean.sh
+++
@@ -1,5 +0,0 @@
-rm -f *.pyc *~
-rm -rf public
-rm -rf gen-py src/gen-js
-rm -rf package-lock.json node_modules
-
